41061e9193fad596b4509102c627c1fa88024fa3
Tomalaya <tomalaya_2@yahoo.com>
2021-08-23 22:11:55 -0500

    Added full power when tail motor is idle (GO FAST)

--- drive.c
+++ drive.c
@@ -8,13 +8,14 @@
  *******************************************************************************/
 /* Drive and movement functions */
 
+float rs,ls,ts;
+JoystickState js;
 
 //This is the functionto control driving the robot. IN this case a 3 Omniwheeled Bot
 void Drive()
 {
 	// This is the robot heading, in radians. Direction is based on X,Y grid, 0 degrees is right,
 	// 90 Stratight ahead ahead, 180 left, and 270 is directly behind.
-	JoystickState js;
 	js.Direction = 0;
 	js.Magnitude = 127; //Motor speed, -127 to 127
 	float rot = 0; //Rotational motoe speed, -127 to 127
@@ -42,13 +43,18 @@ void Drive()
 	}
 
 	// Calculate the speed for each motor by getting its vector for the direction the motor is placed
-	float rs = (js.Magnitude * cos(degreesToRadians(120)-js.Direction)); //Right wheel aims at 120 degrees
-	float ls = (js.Magnitude * cos(degreesToRadians(240)-js.Direction)); //Left aims at 240.
-	float ts = (js.Magnitude * cos(/*degreesToRadians(0)*/-js.Direction)); //Tail wheel aims at 0
+	rs = (js.Magnitude * cos(degreesToRadians(120)-js.Direction)); //Right wheel aims at 120 degrees
+	ls = (js.Magnitude * cos(degreesToRadians(240)-js.Direction)); //Left aims at 240.
+	ts = (js.Magnitude * cos(/*degreesToRadians(0)*/-js.Direction)); //Tail wheel aims at 0
 
+	//Full power!
+	if (abs(ts) < 40 && (abs(rs)>100 || abs(ls)>100)) {
+		int diff = abs(rs) > abs(ls) ? 127-abs(rs) : 127-abs(ls);
+		if (rs>0) rs += diff; else rs -= diff;
+		if (ls>0) ls += diff; else ls -= diff;
+	}
 	// Assign the speed to each motor, mixing velocity and rotation
 	motor[rightMotor] = rs + rot;
 	motor[leftMotor] = ls + rot;
 	motor[tailMotor] = ts + rot;
 }
-
