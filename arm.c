5acc82850c1503a1397d3c3ab2b1058fc2b89a2a
FrostWizard4 <88950263+FrostWizard4@users.noreply.github.com>
2021-09-14 19:21:28 -0500

    Add files via upload

--- arm.c
+++ arm.c
@@ -1,7 +1,9 @@
 task armStart() {
 	while(true) {
 		int armMotorSpeed = 0;
-		//Determine Speed here
+		if(vexRT[Ch2] > 20 && vexRT[Ch2] < -20) {
+			armMotorSpeed = vexRT[Ch2];
+	  }
 		motor[ArmMotor] = 0 + armMotorSpeed;
 	}
 }
--- handServo.c
+++ handServo.c
@@ -1,6 +1,20 @@
 
 task handServoStart() {
-
-
-
+	while(vexRT[Btn6U] == 1) {
+		i++;
+		if(i >= 100) {
+			i = 100;
+		}
+		//Determine Speed here
+		motor[HandServo] = 0 + i;
+	}
+}
+task handServoReverse() {
+	if(i >= 100) {
+			i = 100;
+	}
+	while(vexRT[Btn6D] == 1) {
+		i++;
+		motor[HandServo] = 0 - i;
+	}
 }
