7cf6b60858eeaba62662817706ad8d1a6db7d160
Logan <90361216+LoganE21@users.noreply.github.com>
2021-09-15 19:45:52 -0500

    Update arm.c
    Moved the arm function to it's own task. Changed an and statement to an or.  fixed bug regarding the arm moving by itself.

--- arm.c
+++ arm.c
@@ -1,9 +1,16 @@
+int armCh2 = 0;
 task armStart() {
+	int armMotorSpeed = 0;
 	while(true) {
-		int armMotorSpeed = 0;
-		if(vexRT[Ch2] > 20 && vexRT[Ch2] < -20) {
-			armMotorSpeed = vexRT[Ch2];
+		armCh2 = vexRT[Ch2];
+
+		if(armCh2 > 20 || armCh2 < -20) {
+			armMotorSpeed = armCh2;
+	  }
+	  else {
+	  	armMotorSpeed = 0;
 	  }
 		motor[ArmMotor] = 0 + armMotorSpeed;
+		wait1Msec(20);
 	}
 }
