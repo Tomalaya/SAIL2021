8c67bbb0caf5293de077b553980545c9cf1b38da
FrostWizard4 <88950263+FrostWizard4@users.noreply.github.com>
2021-09-19 14:43:53 -0500

    Add files via upload

--- handServo.c
+++ handServo.c
@@ -1,19 +1,19 @@
 
-task handServoStart() {						 // Start handServoStart task
-	int i = 0; 											 // Initiate i here outside of while loop lexical scope
-	while(true) { 									 // Run forever
-		if(vexRT[Btn6U] == 1) { 			 // If the Button is down, run this:
-		  i++;												 // Add 1 to i here
-		  if(i >= 127) {							 // If i counts to higher than 127,
-			  i = 127;									 // Set i to 127
+task handServoStart() {						 			// Start handServoStart task
+	int i = 0; 											 			// Initiate i here outside of while loop lexical scope
+	while(true) { 									 			// Run forever
+		if(vexRT[Btn6U] == 1) { 			 			// If the Button is down, run this:
+			for(int k = 0; k < 127; k++) {		// Declare int k, if k is greater than -127, subtract 1 from k and,
+				i = k;													// Set j to k
+				wait10Msec(10);									// Wait 100 Milliseconds
 		  }
-		} else if(vexRT[Btn6D] == 1) { // Else, if Button is down, run this:
-			i--;												 // Subtract 1 from i here
-			if(i <= -127) {							 // if i is less than 127, then
-			  i = -127;									 // Set i to -127
-	    }
+		} else if(vexRT[Btn6D] == 1) { 			// Else, if Button is down, run this:
+			for(int k = 0; k > -127; k--) {		// Declare int k, if k is greater than -127, subtract 1 from k and,
+				i = k;													// Set j to k
+				wait10Msec(10);									// Wait 100 Milliseconds
+		  }
 	  }
-		motor[HandServo] = 0 + i;      // Add whatever i is to the motor
+		motor[HandServo] = 0 + i;      			// Add whatever i is to the motor
 	}
-	wait10Msec(20); 								 // wait 200 milliseconds
+	wait10Msec(20); 								 			// wait 200 milliseconds
 }
