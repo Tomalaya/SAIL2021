2acbf2458c15988776d778e324018b0577a5aeba
FrostWizard4 <88950263+FrostWizard4@users.noreply.github.com>
2021-09-19 16:36:12 -0500

    Add files via upload

--- auto.c
+++ auto.c
@@ -2,11 +2,11 @@
 task autoStart() {												// Start task armStart
 		while(true) {													// Loop forever
 		playSound(soundBlip);
-		while(getTimer(T1, 10)) {							// Use a timer as a condition, 10 Seconds
+		while(getTimer(T1, 10) == 10) {							// Use a timer as a condition, 10 Seconds
 			motor[LeftWheel] = 120;							// Set Left Motor to 120
 			motor[RightWheel] = 120;						// Set Right Motor to 120
 	  }
-	  while(getTimer(T2, 10)) {							// Use a timer as a condition, 10 Seconds
+	  while(getTimer(T2, 10) == 10) {							// Use a timer as a condition, 10 Seconds
 			motor[LeftWheel] = -120;						// Set Left Motor to -120
 			motor[RightWheel] = -120;						// Set Right Motor to -120
 	  }
