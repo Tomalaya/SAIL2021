51d121a3ab70758882284fa3ea1a010306380a93
FrostWizard4 <88950263+FrostWizard4@users.noreply.github.com>
2021-09-21 19:57:17 -0500

    Add files via upload

--- auto.c
+++ auto.c
@@ -1,14 +1,17 @@
 
 task autoStart() {												// Start task armStart
-		while(true) {													// Loop forever
-		playSound(soundBlip);
-		while(getTimer(T1, 10) == 10) {							// Use a timer as a condition, 10 Seconds
+		int i = 0;														// Declare Global Integer i
+		int j = 0;														// Declare Global Integer j
+		playSound(soundBlip);									// Play a sound to indicate task is running
+		while(i < 100000) {										// Loop until i hits 100,000
 			motor[LeftWheel] = 120;							// Set Left Motor to 120
 			motor[RightWheel] = 120;						// Set Right Motor to 120
+			i++;																// Increment i by 1
 	  }
-	  while(getTimer(T2, 10) == 10) {							// Use a timer as a condition, 10 Seconds
-			motor[LeftWheel] = -120;						// Set Left Motor to -120
-			motor[RightWheel] = -120;						// Set Right Motor to -120
+	  while(j < 100000) {										// Loop until j hits 100,000
+	  	motor[LeftWheel] = 120;							// Set Left Motor to 120
+	  	motor[RightWheel] = 120;						// Set Right Motor to 120
+	  	j++;																// Increment j by 1
 	  }
 		wait1Msec(20);												// Wait 20 Milliseconds
 	}
