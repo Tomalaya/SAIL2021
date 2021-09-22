60157a9f92423dddeb7bb15eaa3a98efceb69a2e
TJ-Blake <90283058+TJ-Blake@users.noreply.github.com>
2021-09-21 20:26:41 -0400

    Update wristdervoprogram.c

---
+++ wristdervoprogram.c
@@ -0,0 +1,23 @@
+#pragma config(Motor,  port7,           WristServo,    tmotorServoStandard, openLoop)
+
+
+task wristServoStart() {						 		// Start handServoStart task
+	int j = 0; 											 			// Initiate i here outside of while loop lexical scope
+	while(true) { 									 			// Run forever
+		if(vexRT[Btn5U] == 1) { 			 			// If the Button is down, run this:
+
+			for(int k = 0; k < 127; k++) {		// Declare int k, if k is greater than -127, subtract 1 from k and,
+				j = k;													// Set j to k
+				wait10Msec(10);									// Wait 100 Milliseconds
+		  }
+		} else if(vexRT[Btn5D] == 1) { 			// Else, if Button is down, run this:
+
+			for(int k = 0; k > -127; k--) {		// Declare int k, if k is greater than -127, subtract 1 from k and,
+				j = k;													// Set j to k
+				wait10Msec(10);									// Wait 100 Milliseconds
+		  }
+	  }
+		motor[WristServo] = 0 + j;     			// Add whatever i is to the motor
+	}
+	wait10Msec(2); 								 				// wait 20 milliseconds
+}
