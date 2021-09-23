159e6ab6d5b1b6a3a13ac1290a56f68299ab675f
Tomalaya <31702587+Tomalaya@users.noreply.github.com>
2021-09-22 19:26:59 -0500

    Revert "fix"
    This reverts commit d3b8ef835c08c267a94623ddda86ea0bb6d6e237, reversing
    changes made to 1130362c4d3b1f23b87cd5feccbc27132167e605.

--- main.c
+++ main.c
@@ -18,29 +18,25 @@ task main() {																	// Main Task
 	startTask(wristServoStart); 								// Start the wristServoStart Task
 	startTask(handServoStart);  								// Start the handServoStart Task
 	startTask(armStart);												// Start the armStart Task
-	int RT4 = vexRT[Ch4];
-	int RT3 = vexRT[Ch3];
-	int btn8 = vexRT[Btn8U];
-	int btn7 = vexRT[Btn7L];
 	while(true) {																// Start a while loop
-		if(RT3 < 20 && RT3 > -20) {								// Check if the Left Joystick Y Axis is in the middle
+		if(vexRT[Ch3] < 20 && vexRT[Ch3] > -20) {	// Check if the Left Joystick Y Axis is in the middle
 			rightSpeed = 0;													// If the Left Joystick Y Axis is, set rightSpeed to 0
 			leftSpeed = 0;													// If the Left Joystick Y Axis is, set leftSpeed to 0
 		} else {																	// Else, if the Left Joystick Y Axis is NOT,
-			rightSpeed = -RT3;							  			// Set rightSpeed to the (-)value of the Left Joystick Y Axis
-			leftSpeed = -RT3;												// Set leftSpeed to the (-)value of the Left Joystick Y Axis
+			rightSpeed = -vexRT[Ch3];							  // Set rightSpeed to the (-)value of the Left Joystick Y Axis
+			leftSpeed = -vexRT[Ch3];								// Set leftSpeed to the (-)value of the Left Joystick Y Axis
 		}
-		if(RT4 < 20 && RT4 > -20) { 							// Check if the Left Joystick X Axis is in the middle
+		if(vexRT[Ch4] < 20 && vexRT[Ch4] > -20) { // Check if the Left Joystick X Axis is in the middle
 			right2Speed = 0;												// If the Left Joystick X Axis is, set right2Speed to 0
 			left2Speed = 0;													// If the Left Joystick X Axis is, set left2Speed to 0
 		} else {																	// Else, if the Left Joystick X Axis is NOT,
-			right2Speed = RT4;											// Set right2Speed to the (-)value of the Left Joystick X Axis
-			left2Speed = RT4;												// Set left2Speed to the (-)value of the Left Joystick X Axis
+			right2Speed = vexRT[Ch4];								// Set right2Speed to the (-)value of the Left Joystick X Axis
+			left2Speed = vexRT[Ch4];								// Set left2Speed to the (-)value of the Left Joystick X Axis
 		}
-		if(btn8 == 1) {														// If Button 8U is Down,
+		if(vexRT[Btn8U] == 1) {										// If Button 8U is Down,
 			startTask(autoStart);										// Start Task autoStart
 	  }
-	  if(btn7 == 1) {
+	  if(vexRT[Btn7L] == 1) {
 	  	right2Speed = -right2Speed;
 	    left2Speed = -left2Speed;
 	    rightSpeed = -rightSpeed;
