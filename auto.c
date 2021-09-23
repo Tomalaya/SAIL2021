79a34fc57277d8cc595d6c7c1b03486208f9f980
TJ-Blake <90283058+TJ-Blake@users.noreply.github.com>
2021-09-23 16:54:37 -0400

    Update auto.c

--- auto.c
+++ auto.c
@@ -1,7 +1,7 @@
 
 bool autoStop() {
 
-        return vexRT[btn7U], vexRT[Btn7D] || vexRT[Btn7L] || vexRT[Btn7R] || vexRT[Btn8D] || vexRT[Btn8L] || vexRT[Btn8R] || vexRT[Btn6U] || vexRT[Btn6D] || vexRT[Btn5U] || vexRT[Btn5D];
+        return vexRT[Btn7U] || vexRT[Btn7D] || vexRT[Btn7L] || vexRT[Btn7R] || vexRT[Btn8D] || vexRT[Btn8L] || vexRT[Btn8R] || vexRT[Btn6U] || vexRT[Btn6D] || vexRT[Btn5U] || vexRT[Btn5D];
 
 }
 
@@ -9,39 +9,46 @@ bool autoStop() {
 task autoStart() {												// Start task armStart
 	int lastBtn8U = 0;
 	int btn8U = 0;
-	bool(autonomous) = false;
+	bool autonomous = false;
 	while(true) {
 		lastBtn8U = btn8U;
 		btn8U = vexRT[btn8U];
-		autonomous = true;
-			if(btn8U && !lastBtn8U) {
 
-				int end = 0;
-				playSound(soundBlip);									// Play a sound to indicate task is running
+		if(btn8U && !lastBtn8U) {
 
-					motor[LeftWheel] = 120;							// Set Left Motor to 120
-					motor[RightWheel] = 120;						// Set Right Motor to 120
+            autonomous = true;
 
+			int end = 0;
+			playSound(soundBlip);									// Play a sound to indicate task is running
 
-
-			  while(end++<20)   {
-              if (autoStop=true) {autonomous=false; break;}
-			  wait1Msec(100);
-
-			  }
-
-			  end = 0;
-			   while(end++<20)   {
-                            if (autoStop=true) {autonomous=false; break;}
-              			  wait1Msec(100);
-
-                motor[LeftWheel] = -120;							// Set Left Motor to 120
-                motor[RightWheel] = -120;                           // Set Right Motor to 120
+			motor[LeftWheel] = 120;							// Set Left Motor to 120
+			motor[RightWheel] = 120;						// Set Right Motor to 120
 
 
 
-				wait1Msec(20);												// Wait 20 Milliseconds
-		}
+			 while(end++<20)   {
+                if (autoStop()) {autonomous=false; break;}
+			    wait1Msec(100);
 
-	}
+			 }
+
+             motor[LeftWheel] = -120;							// Set Left Motor to 120
+             motor[RightWheel] = -120;                           // Set Right Motor to 120
+
+
+			 end = 0;
+			 while(end++<20)   {
+                if (autoStop()) {autonomous=false; break;}
+                wait1Msec(100);
+
+
+
+
+
+		    }
+
+	    }
+        wait1Msec(20);	                                        //Wait 20 Milliseconds
+    }
+
 }
