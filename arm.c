57e398b2625482e361d4d16b4279b99f1c59686e
FrostWizard4 <88950263+FrostWizard4@users.noreply.github.com>
2021-09-22 19:08:12 -0500

    Add files via upload

--- arm.c
+++ arm.c
@@ -7,10 +7,10 @@ task armStart() {													// Start task armStart
 
 		if(armCh2 > 20 || armCh2 < -20) {			// If armCh2 is greater than 20 or armCh2 is less than -20
 			armMotorSpeed = armCh2;							// Set armMotorSpeed to armCh2
-		}
-		else {																// Else,
-			armMotorSpeed = 0;									// Set armMotorSpeed to 0
-		}
+	  }
+	  else {																// Else,
+	  	armMotorSpeed = 0;									// Set armMotorSpeed to 0
+	  }
 		motor[ArmMotor] = 0 + armMotorSpeed;  // Set Arm Motor to 0 + armMotorSpeed
 		wait1Msec(20);												// Wait 200 Milliseconds
 	}
--- main.c
+++ main.c
@@ -11,41 +11,41 @@
 #include <auto.c>															// Include File auto.c
 
 task main() {																	// Main Task
-	int reverse = 1;
 	int leftSpeed = 0;													// Initialize integer leftSpeed
 	int rightSpeed = 0;													// Initialize integer rightSpeed
 	int left2Speed = 0;													// Initialize integer left2Speed
 	int right2Speed = 0;												// Initialize integer right2Speed
-	int lastBtn8L = 0;
-	int btn8L = 0;
 	startTask(wristServoStart); 								// Start the wristServoStart Task
 	startTask(handServoStart);  								// Start the handServoStart Task
 	startTask(armStart);												// Start the armStart Task
-	startTask(autoStart);
+	int RT4 = vexRT[Ch4];
+	int RT3 = vexRT[Ch3];
+	int btn8 = vexRT[Btn8U];
+	int btn7 = vexRT[Btn7L];
 	while(true) {																// Start a while loop
-		if(vexRT[Ch3] < 20 && vexRT[Ch3] > -20) {	// Check if the Left Joystick Y Axis is in the middle
+		if(RT3 < 20 && RT3 > -20) {								// Check if the Left Joystick Y Axis is in the middle
 			rightSpeed = 0;													// If the Left Joystick Y Axis is, set rightSpeed to 0
 			leftSpeed = 0;													// If the Left Joystick Y Axis is, set leftSpeed to 0
 		} else {																	// Else, if the Left Joystick Y Axis is NOT,
-			rightSpeed = -vexRT[Ch3];							  // Set rightSpeed to the (-)value of the Left Joystick Y Axis
-			leftSpeed = -vexRT[Ch3];								// Set leftSpeed to the (-)value of the Left Joystick Y Axis
+			rightSpeed = -RT3;							  			// Set rightSpeed to the (-)value of the Left Joystick Y Axis
+			leftSpeed = -RT3;												// Set leftSpeed to the (-)value of the Left Joystick Y Axis
 		}
-		if(vexRT[Ch4] < 20 && vexRT[Ch4] > -20) { // Check if the Left Joystick X Axis is in the middle
+		if(RT4 < 20 && RT4 > -20) { 							// Check if the Left Joystick X Axis is in the middle
 			right2Speed = 0;												// If the Left Joystick X Axis is, set right2Speed to 0
 			left2Speed = 0;													// If the Left Joystick X Axis is, set left2Speed to 0
 		} else {																	// Else, if the Left Joystick X Axis is NOT,
-			right2Speed = vexRT[Ch4];								// Set right2Speed to the (-)value of the Left Joystick X Axis
-			left2Speed = vexRT[Ch4];								// Set left2Speed to the (-)value of the Left Joystick X Axis
+			right2Speed = RT4;											// Set right2Speed to the (-)value of the Left Joystick X Axis
+			left2Speed = RT4;												// Set left2Speed to the (-)value of the Left Joystick X Axis
 		}
-		lastBtn8L = btn8L;
-		btn8L = vexRT[btn8L];
-	  if(btn8L && !lastBtn8L) {
-	  	reverse = -1 * reverse;
-    }
-    right2Speed = reverse*right2Speed;
-	  left2Speed = reverse*left2Speed;
-	  rightSpeed = reverse*rightSpeed;
-	  leftSpeed = reverse*leftSpeed;
+		if(btn8 == 1) {														// If Button 8U is Down,
+			startTask(autoStart);										// Start Task autoStart
+	  }
+	  if(btn7 == 1) {
+	  	right2Speed = -right2Speed;
+	    left2Speed = -left2Speed;
+	    rightSpeed = -rightSpeed;
+	    leftSpeed = -leftSpeed;
+		}
 		motor[RightWheel]= 0 + rightSpeed + right2Speed; 		// Add rightSpeed and right2Speed to the Right Wheel Motor
 		motor[LeftWheel]= 0 + (-leftSpeed) - (-left2Speed);	// Add the negative of leftSpeed and add left2Speed to the Left Wheel Motor
 
