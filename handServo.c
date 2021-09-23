a403fa5e0a03f45b2e9b4f32dcb0d9109974f6c7
Logan <90361216+LoganE21@users.noreply.github.com>
2021-09-22 20:02:14 -0500

    Update handServo.c
    Simple hand-servo code updated.

--- handServo.c
+++ handServo.c
@@ -10,16 +10,12 @@ task handServoStart() {						 			// Start handServoStart task
 		btn6U = vexRT[btn6U];
 		lastBtn6D = btn6D;
 		btn6D = vexRT[btn6D];
-		if(btn6U && !lastBtn6U) { 			 			// If the Button is down, run this:
-			for(int k = 0; k < 127000; k++) { // Declare int k, if k is greater than -127, subtract 1 from k and,
-				i = k;													// Set j to k
-				wait1Msec(20);									// Wait 100 Milliseconds
-		  }
-		} else if(btn6U && !lastBtn6U) { 			// Else, if Button is down, run this:
-			for(int k = 0; k > -127000; k--) {// Declare int k, if k is greater than -127, subtract 1 from k and,
-				i = k;													// Set j to k
-				wait1Msec(100);									// Wait 100 Milliseconds
-		  }
+		if(btn6U && !lastBtn6U) { 			 		// If the Button is down, run this:
+
+				i = 127;												// Set i to 127
+
+		} else if(btn6D && !lastBtn6D) { 		// Else, if Button is down, run this:
+				i = -127;													// Set i to 127
 	  }
 		motor[HandServo] = 0 + (i / 100);   // Add whatever i is to the motor
 	}
