db41eefa3cc62e999a2803f7afcf199cef0d7570
Logan <90361216+LoganE21@users.noreply.github.com>
2021-09-29 18:24:14 -0500

    Update handServo.c
    Hand servo has been repaired.

--- handServo.c
+++ handServo.c
@@ -3,24 +3,20 @@ task handServoStart() {						 			// Start handServoStart task
 	int i = 0; 														// Initiate i here outside of while loop lexical scope
 	USEBTN(Btn6U);
 	USEBTN(Btn6D);
+	int lastI = 0;
 	while(true) {  												// Run forever
 		UPDATEBTN(Btn6U);
 		UPDATEBTN(Btn6D);
+		lastI = i;
 		if(GETBTN(Btn6U)) { 			 		// If the Button is down, run this:
-			wait1Msec(10);
 			i+=10;
-			if(i >= 127) {
-				i = 127;
-			}
 		} else if(GETBTN(Btn6D)) { 		// Else, if Button is down, run this:
-
-			wait1Msec(10);
 			i -= 10;
-			if(i <= -127) {
-				i = -127;
-			}
 	  }
-		motor[HandServo] = 0 + i;   // Add whatever i is to the motor
+	  if(i < -127)i = -127; else if(i > 127) i = 127;
+	  if(lastI != i) {
+			motor[HandServo] = i;   // Add whatever i is to the motor
+		}
+		wait1Msec(20);							// wait 20 milliseconds
 	}
-	wait1Msec(20); 								 				// wait 20 milliseconds
 }
