4ba6fa8b6030e6f66b6269b801e1bcd6d95f5ca1
TJ-Blake <90283058+TJ-Blake@users.noreply.github.com>
2021-09-29 19:32:04 -0400

    Update wristServo.c

--- wristServo.c
+++ wristServo.c
@@ -1,33 +1,22 @@
-
-task wristServoStart() {						 		// Start handServoStart task
+task wristServoStart() {						 			// Start wristServoStart task
+	int i = 127; 														// Initiate i here outside of while loop lexical scope
 	USEBTN(Btn5U);
 	USEBTN(Btn5D);
-	USEBTN(Btn8R);
-	int j = 0; 											 			// Initiate i here outside of while loop lexical scope
-
-	while(true) { 									 			// Run forever
+	int lastI = 0;
+	motor[WristServo] = i;
+	while(true) {  												// Run forever
 		UPDATEBTN(Btn5U);
 		UPDATEBTN(Btn5D);
-		UPDATEBTN(Btn8R);
+		lastI = i;
 		if(GETBTN(Btn5U)) { 			 		// If the Button is down, run this:
-			wait1Msec(25);
-			j+=10;
-			if(j >= 127) {
-				j = 127;
-			}
+			i+=10;
 		} else if(GETBTN(Btn5D)) { 		// Else, if Button is down, run this:
-
-			wait1Msec(25);
-			j -= 10;
-			if(j <= -127) {
-				j = -127;
-			}
-	  } else if(BTNPRESSED(Btn8R)) {
-
-	  	j = 0;
-
-		}
-		motor[WristServo] = j;     			// Add whatever j is to the motor
+			i -= 10;
+	    }
+	    if(i < -127)i = -127; else if(i > 127) i = 127;
+	    if(lastI != i) {
+			motor[WristServo] = i;   // Add whatever i is to the motor
+	    }
+		wait1Msec(20);							// wait 20 milliseconds
 	}
-	wait1Msec(20); 								 				// wait 20 milliseconds
 }
