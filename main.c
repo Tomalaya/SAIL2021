85554d632b6088b1b1bd3c7a46bd6ec4321dffde
Logan <90361216+LoganE21@users.noreply.github.com>
2021-09-29 20:03:36 -0500

    Update main.c
    Reversed arm function

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #pragma config(Motor,  port2,           RightWheel,    tmotorServoContinuousRotation, openLoop, reversed, driveLeft)
 #pragma config(Motor,  port3,           LightpoleServo, tmotorServoStandard, openLoop)
-#pragma config(Motor,  port4,           ArmMotor,      tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port4,           ArmMotor,      tmotorServoContinuousRotation, openLoop, reversed)
 #pragma config(Motor,  port5,           HandServo,     tmotorServoStandard, openLoop)
 #pragma config(Motor,  port7,           WristServo,    tmotorServoStandard, openLoop)
 #pragma config(Motor,  port9,           LeftWheel,     tmotorServoContinuousRotation, openLoop)
