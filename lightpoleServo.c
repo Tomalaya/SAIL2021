56ac8eaef8f0677bec1772a2e878d4f4199ae000
TJ-Blake <90283058+TJ-Blake@users.noreply.github.com>
2021-09-30 20:42:26 -0400

    Added full reset, and small nudge to lightpole

--- lightpoleServo.c
+++ lightpoleServo.c
@@ -2,16 +2,24 @@ task lightServoStart() {						 			// Start handServoStart task
 	int i = 0; 														// Initiate i here outside of while loop lexical scope
 	USEBTN(Btn7U);
 	USEBTN(Btn7D);
+	USEBTN(Btn7R);
+	USEBTN(Btn7L);
 	int lastI = 0;
 	while(true) {  												// Run forever
 		UPDATEBTN(Btn7U);
 		UPDATEBTN(Btn7D);
+		UPDATEBTN(Btn7R);
+		UPDATEBTN(Btn7L);
 		lastI = i;
 		if(GETBTN(Btn7U)) { 			 		// If the Button is down, run this:
 			i+=10;
 		} else if(GETBTN(Btn7D)) { 		// Else, if Button is down, run this:
 			i -= 10;
-	  }
+	    } else if(GETBTN(Btn7L)) {      //full reset
+	        i = -127;
+	    } else if(GETBTN(Btn7R)) {
+	        i +=2;
+	    }
 	  if(i < -127)i = -127; else if(i > 127) i = 127;
 	  if(lastI != i) {
 			motor[LightpoleServo] = i;   // Add whatever i is to the motor
