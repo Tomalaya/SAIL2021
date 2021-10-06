3cfa8f2cad603d64626f6943759cdb696119d10a
TJ-Blake <90283058+TJ-Blake@users.noreply.github.com>
2021-10-05 20:11:34 -0500

    Update plow.c

--- plow.c
+++ plow.c
@@ -1,21 +1,17 @@
 
 task plowMotorStart() {						 			// Start handServoStart task
 																				// Initiate i here outside of while loop lexical scope
-		USEBTN(Btn8U);
-		USEBTN(Btn8D);
-		while(true) {  											// Run forever
-		int i = 0;
-		UPDATEBTN(Btn8U);
-		UPDATEBTN(Btn8D);
-		if(GETBTN(Btn8U)) 									// If the Button is down, run this:
-			{
-			i = vexRT[Btn8U];
-		} else {
-			}
-		{
-		i = -vexRT[Btn8U];
-		}
-		motor[PlowMotor] = 0 + i;
-		wait1Msec(20);											// wait 20 milliseconds
+	USEBTN(Btn8U);
+	USEBTN(Btn8D);
+	while(true) {  											// Run forever
+
+	UPDATEBTN(Btn8U);
+	UPDATEBTN(Btn8D);
+	if(GETBTN(Btn8U)) 	{								// If the Button is down, run this:
+		motor[PlowMotor] = 127;
+	} else if(GETBTN(Btn8D))    {
+	    motor[PlowMotor] = -127;
+	}
+	wait1Msec(20);											// wait 20 milliseconds
 	}
 }
