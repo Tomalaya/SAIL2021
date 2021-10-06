2a3fc2dca34a9335f7045e7de847e9fc55cbeae5
lilnige <90291840+lilnige@users.noreply.github.com>
2021-10-05 19:58:13 -0500

    formatting changes and removed some random code more in the description
    I removed a else on line 14 in plow.c.
    I also changed some formatting too

--- plow.c
+++ plow.c
@@ -1,18 +1,21 @@
 
 task plowMotorStart() {						 			// Start handServoStart task
 																				// Initiate i here outside of while loop lexical scope
-	USEBTN(Btn8U);
-	USEBTN(Btn8D);
-	while(true) {  												// Run forever
+		USEBTN(Btn8U);
+		USEBTN(Btn8D);
+		while(true) {  											// Run forever
 		int i = 0;
 		UPDATEBTN(Btn8U);
 		UPDATEBTN(Btn8D);
-		if(GETBTN(Btn8U)) { 			 		// If the Button is down, run this:
+		if(GETBTN(Btn8U)) 									// If the Button is down, run this:
+			{
 			i = vexRT[Btn8U];
 		} else {
-			i = -vexRT[Btn8U];
+			}
+		{
+		i = -vexRT[Btn8U];
 		}
 		motor[PlowMotor] = 0 + i;
-		wait1Msec(20);							// wait 20 milliseconds
+		wait1Msec(20);											// wait 20 milliseconds
 	}
 }
