f94ddbd16ba992acd5a623f44baeaec252514df5
FrostWizard4 <frostwizard4@gmail.com>
2021-10-06 19:29:00 -0500

    Update lightpoleServo.c

--- lightpoleServo.c
+++ lightpoleServo.c
@@ -9,25 +9,29 @@ task lightServoStart() {						 			// Start handServoStart task
 	USEBTN(Btn7R);
 	int lastI = 0;
 	while(true) {  												// Run forever
-		UPDATEBTN(Btn7D);
+	    UPDATEBTN(Btn7D);
 		UPDATEBTN(Btn7U);
 		UPDATEBTN(Btn7L);
 		UPDATEBTN(Btn7R);
 		lastI = i;
 		x = i;
 		if(BTNPRESSED(Btn7U)) { 			 		// If the Button is down, run this:
-			i+=10;
+			i += 10;
 		} else if(BTNPRESSED(Btn7D)) { 		// Else, if Button is down, run this:
 			i -= 10;
 	    } else if(BTNPRESSED(Btn7L)) {      //full reset
-	        i = -127;
+	        i = -67;
 	    } else if(BTNPRESSED(Btn7R)) {
-	        i +=2;
+	        i += 2;
 	    }
-	  if(i < -127)i = -127; else if(i > 127) i = 127;
-	  if(lastI != i) {
-			motor[LightpoleServo] = i;   // Add whatever i is to the motor
-		}
-		wait1Msec(20);							// wait 20 milliseconds
+	    if(i < -67) {
+	        i = -67;
+	    } else if(i > 103) {
+	        i = 103;
+	    }
+	    if(lastI != i) {
+	        motor[LightpoleServo] = i;   // Add whatever i is to the motor
+        }
+	    wait1Msec(20);							// wait 20 milliseconds
 	}
 }
